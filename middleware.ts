0e6add12d79bfb1f3506b8287fb0924c792d972e
h-ouch <2439062657@qq.com>
2024-09-24 22:18:10 +0800

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
-export default clerkMiddleware();
 
+const isWebhookRoute = createRouteMatcher(['/api/webhooks/clerk'])
+export default clerkMiddleware((auth, req) => {
+  if (!isWebhookRoute(req)) {
+    auth().protect()
+  }
+})
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
